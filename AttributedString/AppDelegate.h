19f8bcaa57f46972c5017b04863a13adec131cda
钟弘崮 <1468128053@qq.com>
2020-01-03 09:51:20 +0800

    没修改什么

--- AttributedString/AppDelegate.h
+++ AttributedString/AppDelegate.h
@@ -3,7 +3,7 @@
 //  AttributedString
 //
 //  Created by Hertz Goo on 2019/11/30.
-//  Copyright © 2019 Hertz Goo. All rights reserved.
+//  Copyright © 2019 Hertz Goo. All rights reserved.  
 //
 
 #import <UIKit/UIKit.h>
